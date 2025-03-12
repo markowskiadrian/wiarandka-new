30a81085afe230e6e704da720149e5a499cd3bef
markowskiadrian <adrianjanmarkowski@gmail.com>
2025-03-12 18:50:58 +0100

    stworzenie okna do logowania
    wprowadzenie wartosci domyslnych, praca nad css

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 session_start();
 include 'config.php';
 
-if(isset($_POST['login'])) {
+if (isset($_POST['login'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
@@ -10,8 +10,8 @@ if(isset($_POST['login'])) {
     $stmt->execute([$username]);
     $user = $stmt->fetch();
 
-    if($user && password_verify($password, $user['password'])) {
-        if($user['is_banned']) {
+    if ($user && password_verify($password, $user['password'])) {
+        if ($user['is_banned']) {
             echo "Your account has been banned!";
         } else {
             $_SESSION['user_id'] = $user['user_id'];
@@ -25,16 +25,44 @@ if(isset($_POST['login'])) {
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-    <title>Wiarandka - Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wiarandka - login</title>
+
+    <link rel="stylesheet" href="css/default.css">
+    <link rel="stylesheet" href="css/login.css">
 </head>
+
 <body>
-    <h2>Login</h2>
-    <form method="POST">
-        <input type="text" name="username" placeholder="Username" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <button type="submit" name="login">Login</button>
-    </form>
-    <p>Don't have an account? <a href="register.php">Register here</a></p>
+    <section>
+        <h2>Zaloguj się <br>do <strong>Wiarandki</strong></h2>
+        <form method="POST">
+            <p>Login</p>
+            <input type="text" name="username" required class="rounded">
+            <p>Hasło</p>
+            <input type="password" name="password" required class="rounded">
+            <a href="register.php">
+                <p>Nie pamiętam hasła</p>
+            </a>
+            <button type="submit" name="login" class="rounded"><span><i class="bi bi-check-all"></i>
+                    Login</span></button>
+            <a href="register.php" id="register">
+                <p>Albo załóż konto</p>
+            </a>
+        </form>
+
+        <div>
+            <p>Lub zaloguj się przez:</p>
+            <div>
+                <i class="bi bi-apple"></i>
+                <i class="bi bi-google"></i>
+                <i class="bi bi-facebook"></i>
+            </div>
+        </div>
+
+    </section>
 </body>
+
 </html>

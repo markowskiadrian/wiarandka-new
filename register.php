3683f7222172dc51a9d5b54ebea1c8148d2333be
markowskiadrian <adrianjanmarkowski@gmail.com>
2025-03-16 13:08:43 +0100

    dodanie profilowych
    formularz rejestracji umożliwia teraz dodanie zdjęcia profilowego, a sekcja swipe'owania wyświetla je poprawnie

--- register.php
+++ register.php
@@ -5,8 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wiarandka - login</title>
-
+    <title>Wiarandka - rejestracja</title>
     <link rel="stylesheet" href="css/default.css">
     <link rel="stylesheet" href="css/login.css">
 </head>
@@ -15,9 +14,15 @@
     <section>
         <p class="close"><a href="login.php"><i class="bi bi-x"></i></a></p>
         <h2>Dołącz do<br><strong>Wiarandki</strong></h2>
-        <form method="POST">
-            <p>Nazwa użytkownika</p>
-            <input type="text" name="username" required class="rounded">
+        <form method="POST" enctype="multipart/form-data">
+            <p>Nazwa użytkownika i zdjęcie profilowe</p>
+            <div id="user">
+                <input type="text" name="username" required class="rounded">
+                <label class="custom-file-upload">
+                    <input type="file" name="profile_pic" accept="image/*" required>
+                    <p><i class="bi bi-person-square"></i></p>
+                </label>
+            </div>
             <p>Hasło</p>
             <input type="password" name="password" required class="rounded">
             <p>E-mail</p>
@@ -38,11 +43,9 @@
             <p class="dark">Opis profilu</p>
             <textarea name="bio" class="rounded2"></textarea>
             <button type="submit" name="register" class="rounded" id="register"><span><i class="bi bi-check-all"></i>
-            Zarejestruj się</span></button>
+                    Zarejestruj się</span></button>
         </form>
     </section>
-
-
     <?php
     if (isset($_POST['register'])) {
         $username = $_POST['username'];
@@ -52,12 +55,26 @@
         $denomination = $_POST['denomination'];
         $bio = $_POST['bio'];
         $gender = $_POST['gender'];
-
-        $stmt = $pdo->prepare("INSERT INTO users (username, password, email, gender, religion, denomination, bio) 
-                              VALUES (?, ?, ?, ?, ?, ?, ?)");
-        $stmt->execute([$username, $password, $email, $gender, $religion, $denomination, $bio]);
-
-        Header("Location: login.php");
+        $target_dir = "profilepics/";
+        $target_file = $target_dir . basename($_FILES["profile_pic"]["name"]);
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+        $check = getimagesize($_FILES["profile_pic"]["tmp_name"]);
+        if ($check !== false) {
+            if ($imageFileType == "jpg" || $imageFileType == "png" || $imageFileType == "jpeg") {
+                if (move_uploaded_file($_FILES["profile_pic"]["tmp_name"], $target_file)) {
+                    $stmt = $pdo->prepare("INSERT INTO users (username, password, email, gender, religion, denomination, bio, profile_pic) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+                    $stmt->execute([$username, $password, $email, $gender, $religion, $denomination, $bio, $target_file]);
+                    header("Location: login.php");
+                    exit();
+                } else {
+                    echo "Wystąpił problem podczas przesyłania zdjęcia profilowego.";
+                }
+            } else {
+                echo "Nieprawidłowy format zdjęcia. Dozwolone są: JPG, JPEG, PNG.";
+            }
+        } else {
+            echo "Przesłany plik nie jest prawidłowym obrazem.";
+        }
     }
     ?>
 </body>
